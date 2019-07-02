0439d0f25869879f4ef04853cf0738a72e489974
TheoT <trolle.theo@gmail.com>
2019-07-02 17:02:42 +0200

    Modif CSS appli

--- www/js/controller.js
+++ www/js/controller.js
@@ -385,6 +385,7 @@ function recuperation_en_ligne_mes_kidzz_callback(response) {
         kidzz = response;
         for (var i = 0; i < response.length; i++) {
             document.getElementById('kidzz_list').appendChild(create_element('div', 'kidzz_' + response[i]['id'], 'kidzz_div_list'));
+            document.getElementById('kidzz_' + response[i]['id']).appendChild(create_element('i', 'id_button_star_'+i,'fas fa-star button-star',"", ""));
             document.getElementById('kidzz_' + response[i]['id']).appendChild(create_element('p', 'id_kdzz_name_' + i, 'kidzz_div_list_item kidzz_div_list_item_name', '', response[i]['nom']));
             document.getElementById('kidzz_' + response[i]['id']).appendChild(create_element('i', 'id_button_play_'+i,'fas fa-play button-play','choix_kidzz('+response[i]['id']+')', ""));
             document.getElementById('kidzz_' + response[i]['id']).appendChild(create_element('p', 'id_kdzz_description_' + i, 'kidzz_div_list_item kidzz_div_list_item_description', '', response[i]['description']));
